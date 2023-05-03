69dc3460d17c18f5cef41ae584c4a5890bcf1340
Ricardo Coronel <montotox@gmail.com>
2023-05-03 19:02:20 +0500

    add language detect

--- __main__.py
+++ __main__.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
 from slack_sdk import WebClient
-from slack_sdk_errors import SlackApiError
+from slack_sdk.errors import SlackApiError
+from langdetect import detect
 
 # Load the token from .env file
 load_dotenv()
@@ -9,11 +10,15 @@ load_dotenv()
 # Create a slack client
 client = WebClient(token=os.environ.get("SLACK_TOKEN"))
 
-try:
-    response = client.chat_postMessage(
-        channel="#testing-bot",
-        text="Recuerda que hoy es día de escribir en inglés"
-    )
-    print("Message sent successfully", response["ts"])
-except SlackApiError as e:
-    print("Error creating conversation: {}".format(e))
+# Detect message language
+def detect_language(text):
+    if "text" in text:
+        lang = detect(text["text"])
+        if lang != "en":
+            try:
+                response = client.chat_postMessage(
+                channel="#testing-bot",
+                text="Recuerda que hoy es día de escribir en inglés" )
+                print("Message sent successfully", response["ts"])
+            except SlackApiError as e:
+                print("Error creating conversation: {}".format(e))
